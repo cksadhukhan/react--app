8665c2147f64a95c5661172a1174f5ed3e658c0d
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2023-01-05 19:06:47 +0530

    component index update

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import BlankCard from "./blank-card.component";
+import Loading from "./loading.component";
+import UserCard from "./user-card.component";
+
+export { BlankCard, Loading, UserCard };
