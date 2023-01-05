4cff111453dac157cb2e3f4c28b8cf09dcd09956
Chanchal Sadhukhan <cksadhukhan@gmail.com>
2023-01-05 19:06:19 +0530

    loading component added

---
+++ src/components/loading.component.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Bars } from "react-loader-spinner";
+
+const Loading: React.FC = () => {
+  return (
+    <div style={styles.container}>
+      <Bars
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="bars-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
+};
+
+export default Loading;
+
+const styles = {
+  container: {
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+};
